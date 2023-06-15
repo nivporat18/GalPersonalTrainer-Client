cf858cd1a49fb58c823c5eda204f41ab4fc97700
Niv Porat <nivporat18@icloud.com>
2023-06-16 00:20:05 +0300

    update home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,7 @@ import logo from "../images/logoremovebgpreview.png"
 const Home = () => {
   return (
 
-<section className="fixed justify-center overflow-hidden bg-[url(https://res.cloudinary.com/mepelaozen/image/upload/v1686810065/GalWeb/gym_background_t0tg5v.jpg)] bg-cover  bg-no-repeat mt-8">
+<section className="fixed  justify-center overflow-hidden bg-[url(https://res.cloudinary.com/mepelaozen/image/upload/v1686810065/GalWeb/gym_background_t0tg5v.jpg)] bg-cover  bg-no-repeat mt-8">
   <div className="bg-black/25 md:p-12 lg:px-16 lg:py-4">
     <div className="text-center ltr:sm:text-left rtl:sm:text-right">
       <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl flex justify-center"><img src={logo} alt="לוגו" className="w-[200px]"/></h2>
