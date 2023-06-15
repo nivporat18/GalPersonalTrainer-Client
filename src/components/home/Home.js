de049d852735d157282d53dc3e061ff9e58d92fc
Niv Porat <nivporat18@icloud.com>
2023-06-16 00:46:09 +0300

    changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,12 +6,12 @@ import logo from "../images/logoremovebgpreview.png"
 const Home = () => {
   return (
 
-<section className="fixed  justify-center overflow-hidden bg-[url(https://res.cloudinary.com/mepelaozen/image/upload/v1686810065/GalWeb/gym_background_t0tg5v.jpg)] bg-cover  bg-no-repeat mt-8">
-  <div className="bg-black/25 md:p-12 lg:px-16 lg:py-4">
-    <div className="text-center ltr:sm:text-left rtl:sm:text-right">
+<section className="flex justify-center overflow-hidden bg-[url(https://res.cloudinary.com/mepelaozen/image/upload/v1686810065/GalWeb/gym_background_t0tg5v.jpg)] min-h-screen bg-cover bg-no-repeat bg-fixed ">
+  <div className="bg-black/25 md:p-12 lg:px-16 lg:py-4 fixed">
+    <div className="text-center ltr:sm:text-left rtl:sm:text-right ">
       <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl flex justify-center"><img src={logo} alt="לוגו" className="w-[200px]"/></h2>
 
-      <p className=" max-w-lg text-white/90  md:block md:text-lg md:leading-relaxed">
+      <p className=" max-w-lg text-white/90   md:text-lg md:leading-relaxed">
     שמי גל פורת, מאמן כושר אישי.<br></br>
 אני רוצה להביא מניסיוני לא רק כמאמן, אלה גם מהצד שלכם כמתאמן רב שנים בחדר הכושר ובעולם הכדורגל.
 כל אחד ואחת מכם בין אם ספורטאים שרוצים לשפר את היכולות הפיזיות והאתלטיות שלהם ובין אם אתם רוצים לעלות מסת שריר ולהתחטב! 
